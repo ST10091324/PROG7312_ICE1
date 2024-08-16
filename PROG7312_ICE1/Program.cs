685225013a3ab3e8d0307a2235a65dea6d4100c0
Eben Mwema <104417932+ST10091324@users.noreply.github.com>
2024-08-16 15:50:19 +0200

    First commit

---
+++ PROG7312_ICE1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG7312_ICE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
